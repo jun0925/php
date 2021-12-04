25761b9b6bf82a347413f46de4e4d4dfbe963ef2
jun0925 <yeongjun0925@gmail.com>
2021-12-04 15:45:00 +0900

    DB접속 코드 작성

---
+++ db/connectDB.php
@@ -0,0 +1,13 @@
+<?php 
+$host = "localhost";
+$user = "root";
+$pw = "111111";
+$dbName = "myapp";
+$dbConnect = new mysqli($host, $user, $pw, $dbName);
+$dbConnect->set_charset("utf-8");
+
+if(mysqli_connect_errno()){
+    echo "데이터베이스 접속 실패";
+    echo mysqli_connect_error();
+}
+?>
