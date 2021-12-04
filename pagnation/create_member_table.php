d4dbdccf756fe03d6257626aa8254fad64518b32
jun0925 <yeongjun0925@gmail.com>
2021-12-04 16:23:46 +0900

    member 테이블 생성

---
+++ pagnation/create_member_table.php
@@ -0,0 +1,20 @@
+<?php 
+include_once "../db/connectDB.php";
+
+$sql = "CREATE TABLE member(
+    id int NOT NUll PRIMARY KEY AUTO_INCREMENT,
+	user_id varchar(10) auto_increment,
+    name VARCHAR(10) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    gender ENUM('m','w','x') DEFAULT 'x',
+	email VARCHAR(255) NOT NULL,
+    reg_time TIMESTAMP NOT NULL
+)";
+
+$result = $dbConnect->query($sql);
+if($result){
+    echo "member 테이블이 생성됐습니다.";
+}else{
+    echo "member 테이블 생성에 실패했습니다.";
+}
+?>
