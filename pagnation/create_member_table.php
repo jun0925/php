ea42d7916478f6b9b756f7fec258a3c21f098e73
jun0925 <31940359+jun0925@users.noreply.github.com>
2021-12-07 08:39:18 +0900

    user_id의 잘못된 옵션값 사용, 테이블 생성시 에러가 발생하면 에러를 화면에 노출시킴

--- pagnation/create_member_table.php
+++ pagnation/create_member_table.php
@@ -3,7 +3,7 @@ include_once "../db/connectDB.php";
 
 $sql = "CREATE TABLE member(
     id int NOT NUll PRIMARY KEY AUTO_INCREMENT,
-	user_id varchar(10) auto_increment,
+	user_id varchar(10) not null,
     name VARCHAR(10) NOT NULL,
     password VARCHAR(50) NOT NULL,
     gender ENUM('m','w','x') DEFAULT 'x',
@@ -15,6 +15,7 @@ $result = $dbConnect->query($sql);
 if($result){
     echo "member 테이블이 생성됐습니다.";
 }else{
-    echo "member 테이블 생성에 실패했습니다.";
+    echo "member 테이블 생성에 실패했습니다.<br />";
+    echo $dbConnect->error;
 }
 ?>
